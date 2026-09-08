2705588208175f88acce2bcb2cc81fe08f9aa5a6
sivert-svanes <sivertss@uia.no>
2026-09-08 16:40:06 +0200

    dev: added a few post and push methods

--- Csharp-API-Test/Csharp-API-Test/Program.cs
+++ Csharp-API-Test/Csharp-API-Test/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseInMemoryDatabase("TodoList"));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
